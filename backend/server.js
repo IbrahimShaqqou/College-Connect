356d4fc46665cd7c8d47b269c3f86b5f5f8f01f8
ahmadshaqqou <108443580+ahmadshaqqou@users.noreply.github.com>
2023-07-16 16:30:46 -0400

    mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,29 @@
+    require('dotenv').config();
+    const mongoose = require('mongoose');
+    const cors = require('cors');
+    const bcrypt = require('bcrypt');
+    const cookieparser = require('cookie-parser');
     const express = require('express');
 
+    
+
     const app = express();
-    const port = 5000;
+    app.use(express.json());
+    app.use(cors());
+    app.use(cookieparser());
+
+    const port = process.env.PORT || 5000;
+    // const URL = process.env.MONGO_URI;
+
+    // mongoose.connect(uri,{
+    //     useCreateIndex: true,
+    //     useFindAndModify: false,
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    // }, err=>{
+    //     if(err) throw err;
+    //     console.log('db is connected')
+    // })
 
     app.get("", (req,res)=>{
         res.status(500).send("hello WRLD");
