d6a5fe3c5d229bc599eaa478425dff23aed979d1
ahmadshaqqou <108443580+ahmadshaqqou@users.noreply.github.com>
2023-07-16 17:19:59 -0400

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 
+import "./index.css";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Register from "./pages/Register";
+import Login from "./pages/login";
+import Post from "./pages/post";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Testing</h1>
-    </div>
-  );
+  return <div className="App">
+      <Router>
+        <Switch>
+        <Route exact path="/register">
+          <Register/>
+        </Route>
+      <Route exact path="/login">
+        <Login/>
+      </Route>
+      <Route exact path="./post/:id">
+        <Post/>
+      </Route>
+      <Route> <NotFound/>   </Route>
+      </Switch>
+      </Router>
+    </div>;
 }
 
 export default App;
---
+++ client/src/pages/post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const post = () => {
+  return (
+    <div>
+        <h3> single post </h3>
+    </div>
+  );
+};
+
+export default post;
