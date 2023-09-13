e56c9f414a5d3d4bcf06a1eb90745914ad9ffecd
ahmadshaqqou <108443580+ahmadshaqqou@users.noreply.github.com>
2023-09-12 22:57:33 -0400

    register changes

--- backend/controllers/authCtrl.js
+++ backend/controllers/authCtrl.js
@@ -6,7 +6,25 @@ const authCtrl = {
     register: async(req,res)=>{
         try{
             const {fullname, username, email, password, gender} = req.body;
-            console.log(req.body);
+            const newUsername = username.toLowerCase().replace(/ /g,'');
+            console.log(newUsername)
+            const user_name = await Users.findOne({username: newUsername})
+           
+            if (user_name) return res.status(400).json({msg: 'this username already exists'})
+
+            const Email = await  Users.findOne({email: email})
+            if (Email)  return res.status(400).json({msg: 'this email already exists'})
+
+            if (password.length < 6) return res.status(400).json({msg: "password must be at least 6 characters long."})
+
+            const passwordHash = await bcrypt.hash(password,13);
+
+            const newUser = new Users({
+                fullname, username:newUsername, email, password:passwordHash, gender
+            })
+
+            console.log(newUser)
+
             res.json({
                 msg:"registerd sucess"
             })
