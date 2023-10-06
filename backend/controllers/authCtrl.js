f2b68f6dd0e0c086f6bdd09564faee739862e03d
ahmadshaqqou <108443580+ahmadshaqqou@users.noreply.github.com>
2023-10-06 00:37:01 -0400

    login, log out, refresh and acress token tested on postman api

--- backend/controllers/authCtrl.js
+++ backend/controllers/authCtrl.js
@@ -8,7 +8,7 @@ const authCtrl = {
             const {fullname, username, email, password, gender} = req.body;
 
             const newUsername = username.toLowerCase().replace(/ /g,'');
-            console.log("New username: " + newUsername);
+            console.log(newUsername);
 
             const user_name = await Users.findOne({username: newUsername})
            
@@ -29,12 +29,28 @@ const authCtrl = {
                 gender: gender,
             })
 
-            console.log(newUser)
+            
 
+            const access_token = createAccessToken({id: newUser._id});
+            const refresh_token = createRefreshToken({id: newUser._id});
+
+
+            res.cookie('refreshtoken',refresh_token,{
+                httpOnly: true,
+                path:"/api/refresh_token",
+                maxAge: 24*30*60*60*1000
+            })
             await newUser.save();
 
+
             res.json({
-                msg:"register success"
+                msg:"register success",
+                access_token,
+                user:{
+                ...newUser._doc,
+                password:''
+                }
+                
             })
 
         }catch(err) {
@@ -43,6 +59,37 @@ const authCtrl = {
     },
     login: async(req,res)=>{
         try{
+            const {email , password} = req.body;
+
+            const user = await Users.findOne({email})
+            .populate("friends following" , "-password")
+
+            if(!user) return res.status(400).json({msg: 'User does not exist'})
+
+            const isMatch = await bcrypt.compare(password,user.password);
+            if(!isMatch) return res.status(400).json({msg: 'User password is incorrect.'})
+
+            const access_token = createAccessToken({id: user._id});
+            const refresh_token = createRefreshToken({id: user._id});
+
+
+            res.cookie('refreshtoken',refresh_token,{
+                httpOnly: true,
+                path:"/api/refresh_token",
+                maxAge: 24*30*60*60*1000
+            })
+
+
+            res.json({
+                msg:"login success",
+                access_token,
+                user:{
+                ...user._doc,
+                password:''
+                }
+                
+            })
+
 
         }catch(err) {
             res.status(500).json({msg: err.message})
@@ -50,18 +97,45 @@ const authCtrl = {
     },
     logout: async(req,res)=>{
         try{
-
+            res.clearCookie('refreshtoken',{path:"/api/refresh_token"})
+            res.json({msg:"Logged out"})
         }catch(err) {
             res.status(500).json({msg: err.message})
         }
     },
     generateAccessToken: async(req,res)=>{
         try{
+            const rf_token = req.cookies.refreshtoken;
+            console.log(rf_token)
 
+            if(!rf_token) return res.status(400).json({msg:"please login now"})
+
+            jwt.verify(rf_token, process.env.REFRESHTOKENSECRET, async(err, result)=>{
+                if(err) return res.status(400).json({msg:"Please login now"})
+                
+                const user = await Users.findById(result.id).select("-password")
+                .populate("friends following")
+
+                if(!user) return res.status(400).json({msg:"user does not exist"})
+
+                const access_token = createAccessToken({id: result._id});
+
+                res.json({
+                    access_token,
+                    user
+                })
+                
+            })
         }catch(err) {
             res.status(500).json({msg: err.message})
         }
     },
 }
 
+const createAccessToken = (  payload) =>{
+    return jwt.sign(payload, process.env.ACCESSTOKENSECRET, {expiresIn:"1d"})
+}
+const createRefreshToken = ( payload) =>{
+    return jwt.sign(payload, process.env.REFRESHTOKENSECRET, {expiresIn: "30d"})
+}
 module.exports = authCtrl
